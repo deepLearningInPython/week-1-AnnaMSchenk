e9fd8353802f5e74bee96d20a3bf2ad4fc9329fa
AnnaMSchenk <127799369+AnnaMSchenk@users.noreply.github.com>
2025-11-03 11:38:41 +0100

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -13,7 +13,13 @@ import numpy
 # Your code here:
 # -----------------------------------------------
 
-def step
+def step(x):
+    if x > 0:
+        return(1)
+    elif x < 0:
+        return(-1)
+    elif x == 0:
+        return(0)
 
 
 # -----------------------------------------------
